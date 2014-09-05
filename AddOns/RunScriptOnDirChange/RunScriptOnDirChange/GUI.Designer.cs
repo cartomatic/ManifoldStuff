ebba6402a2d35348018486663b069bb2829211de
Dominik Mikiewicz <dominik@cartomatic.pl>
2014-09-05 12:12:07 +0200

    Add example of manifold & FileFystemWatcher integration

---
+++ AddOns/RunScriptOnDirChange/RunScriptOnDirChange/GUI.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace RunScriptOnDirChange
+{
+    partial class GUI
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnStartWatcher = new System.Windows.Forms.Button();
+            this.btnStopWatcher = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowseDir = new System.Windows.Forms.Button();
+            this.cmbScripts = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chckBoxCreate = new System.Windows.Forms.CheckBox();
+            this.chckBoxChange = new System.Windows.Forms.CheckBox();
+            this.chckBoxDelete = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDumpName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDir
+            // 
+            this.txtDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDir.Location = new System.Drawing.Point(3, 21);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.ReadOnly = true;
+            this.txtDir.Size = new System.Drawing.Size(155, 20);
+            this.txtDir.TabIndex = 0;
+            // 
+            // btnStartWatcher
+            // 
+            this.btnStartWatcher.Location = new System.Drawing.Point(3, 181);
+            this.btnStartWatcher.Name = "btnStartWatcher";
+            this.btnStartWatcher.Size = new System.Drawing.Size(93, 23);
+            this.btnStartWatcher.TabIndex = 1;
+            this.btnStartWatcher.Text = "Start";
+            this.btnStartWatcher.UseVisualStyleBackColor = true;
+            this.btnStartWatcher.Click += new System.EventHandler(this.btnStartWatcher_Click);
+            // 
+            // btnStopWatcher
+            // 
+            this.btnStopWatcher.Enabled = false;
+            this.btnStopWatcher.Location = new System.Drawing.Point(102, 181);
+            this.btnStopWatcher.Name = "btnStopWatcher";
+            this.btnStopWatcher.Size = new System.Drawing.Size(89, 23);
+            this.btnStopWatcher.TabIndex = 2;
+            this.btnStopWatcher.Text = "Stop";
+            this.btnStopWatcher.UseVisualStyleBackColor = true;
+            this.btnStopWatcher.Click += new System.EventHandler(this.btnStopWatcher_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Directory to watch for changes:";
+            // 
+            // btnBrowseDir
+            // 
+            this.btnBrowseDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseDir.Location = new System.Drawing.Point(164, 21);
+            this.btnBrowseDir.Name = "btnBrowseDir";
+            this.btnBrowseDir.Size = new System.Drawing.Size(27, 20);
+            this.btnBrowseDir.TabIndex = 4;
+            this.btnBrowseDir.Text = "...";
+            this.btnBrowseDir.UseVisualStyleBackColor = true;
+            this.btnBrowseDir.Click += new System.EventHandler(this.btnBrowseDir_Click);
+            // 
+            // cmbScripts
+            // 
+            this.cmbScripts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbScripts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbScripts.FormattingEnabled = true;
+            this.cmbScripts.Location = new System.Drawing.Point(3, 110);
+            this.cmbScripts.Name = "cmbScripts";
+            this.cmbScripts.Size = new System.Drawing.Size(188, 21);
+            this.cmbScripts.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Callback script:";
+            // 
+            // chckBoxCreate
+            // 
+            this.chckBoxCreate.AutoSize = true;
+            this.chckBoxCreate.Checked = true;
+            this.chckBoxCreate.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chckBoxCreate.Location = new System.Drawing.Point(5, 68);
+            this.chckBoxCreate.Name = "chckBoxCreate";
+            this.chckBoxCreate.Size = new System.Drawing.Size(56, 17);
+            this.chckBoxCreate.TabIndex = 7;
+            this.chckBoxCreate.Text = "create";
+            this.chckBoxCreate.UseVisualStyleBackColor = true;
+            // 
+            // chckBoxChange
+            // 
+            this.chckBoxChange.AutoSize = true;
+            this.chckBoxChange.Checked = true;
+            this.chckBoxChange.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chckBoxChange.Location = new System.Drawing.Point(67, 68);
+            this.chckBoxChange.Name = "chckBoxChange";
+            this.chckBoxChange.Size = new System.Drawing.Size(62, 17);
+            this.chckBoxChange.TabIndex = 8;
+            this.chckBoxChange.Text = "change";
+            this.chckBoxChange.UseVisualStyleBackColor = true;
+            // 
+            // chckBoxDelete
+            // 
+            this.chckBoxDelete.AutoSize = true;
+            this.chckBoxDelete.Location = new System.Drawing.Point(136, 68);
+            this.chckBoxDelete.Name = "chckBoxDelete";
+            this.chckBoxDelete.Size = new System.Drawing.Size(55, 17);
+            this.chckBoxDelete.TabIndex = 9;
+            this.chckBoxDelete.Text = "delete";
+            this.chckBoxDelete.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Changes to watch for:";
+            // 
+            // txtDumpName
+            // 
+            this.txtDumpName.Location = new System.Drawing.Point(3, 155);
+            this.txtDumpName.Name = "txtDumpName";
+            this.txtDumpName.Size = new System.Drawing.Size(188, 20);
+            this.txtDumpName.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Name of the watcher notification comp:";
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDumpName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chckBoxDelete);
+            this.Controls.Add(this.chckBoxChange);
+            this.Controls.Add(this.chckBoxCreate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbScripts);
+            this.Controls.Add(this.btnBrowseDir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStopWatcher);
+            this.Controls.Add(this.btnStartWatcher);
+            this.Controls.Add(this.txtDir);
+            this.Name = "GUI";
+            this.Size = new System.Drawing.Size(194, 262);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnStartWatcher;
+        private System.Windows.Forms.Button btnStopWatcher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowseDir;
+        private System.Windows.Forms.ComboBox cmbScripts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chckBoxCreate;
+        private System.Windows.Forms.CheckBox chckBoxChange;
+        private System.Windows.Forms.CheckBox chckBoxDelete;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDumpName;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ AddOns/RunScriptOnDirChange/RunScriptOnDirChange/GUI.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using M = Manifold.Interop;
+using System.IO;
+
+namespace RunScriptOnDirChange
+{
+    public partial class GUI : UserControl, Manifold.Interop.Scripts.IEventsConnection
+    {
+
+        /// <summary>
+        /// mapp
+        /// </summary>
+        protected M.Application mApp { get; set; }
+
+        /// <summary>
+        /// File system watcher instance
+        /// </summary>
+        protected FileSystemWatcher watcher { get; set; }
+
+        /// <summary>
+        /// name of a callback script to be executed on change
+        /// </summary>
+        protected string callbackScript { get; set; }
+
+        public GUI()
+        {
+            InitializeComponent();
+            txtDumpName.Text = watcherNotificationsComp;
+
+            //dump some data to a variable, so it can be accessed from a non-ui thread
+            cmbScripts.SelectedValueChanged += cmbScripts_SelectedValueChanged;
+        }
+
+        /// <summary>
+        /// connect M events listeners
+        /// </summary>
+        /// <param name="ev"></param>
+        public void ConnectEvents(Manifold.Interop.Scripts.Events ev)
+        {
+            //watch for doc cnahges
+            ev.DocumentClosed += new Manifold.Interop.Scripts.Events.DocumentClosedEventHandler(DocumentChanged);
+            ev.DocumentCreated += new Manifold.Interop.Scripts.Events.DocumentCreatedEventHandler(DocumentChanged);
+            ev.DocumentOpened += new Manifold.Interop.Scripts.Events.DocumentOpenedEventHandler(DocumentChanged);
+            ev.DocumentSaved += new Manifold.Interop.Scripts.Events.DocumentSavedEventHandler(DocumentChanged);
+            ev.AddinLoaded += new Manifold.Interop.Scripts.Events.AddinLoadedEventHandler(DocumentChanged);
+
+
+            //watch for component changes
+            ev.ComponentsRemoved += ev_ComponentsRemoved;
+            ev.ComponentsAdded += ev_ComponentsAdded;
+            ev.ComponentNameChanged += ev_ComponentNameChanged;
+        }
+
+        /// <summary>
+        /// cmbScripts_SelectedValueChanged callback
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void cmbScripts_SelectedValueChanged(object sender, EventArgs e)
+        {
+            callbackScript = cmbScripts.SelectedValue as string;
+        }
+
+        
+        void ev_ComponentNameChanged(object sender, M.Scripts.ComponentEventArgs Args)
+        {
+            BindScriptsCombo();
+        }
+
+        void ev_ComponentsAdded(object sender, M.Scripts.DocumentEventArgs Args)
+        {
+            BindScriptsCombo();
+        }
+
+        void ev_ComponentsRemoved(object sender, M.Scripts.DocumentEventArgs Args)
+        {
+            BindScriptsCombo();
+        }
+
+
+        private string noScript = "Ignore callbacks...";
+
+        /// <summary>
+        /// loads available scripts to combobox
+        /// </summary>
+        private void BindScriptsCombo()
+        {
+            var comboData = new List<string>();
+            comboData.Add(noScript);
+
+            try //when closing document, this sometimes gets unhappy
+            {
+                if (mApp != null && mApp.ActiveDocument != null)
+                {
+                    foreach (M.Component c in mApp.ActiveDocument.ComponentSet)
+                    {
+                        if (c.Type == M.ComponentType.ComponentScript)
+                        {
+                            comboData.Add(c.Name);
+                        }
+                    }
+                }
+            }
+            catch { }
+
+            cmbScripts.DataSource = comboData;
+        }
+
+        /// <summary>
+        /// doc changed callback
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void DocumentChanged(System.Object sender, Manifold.Interop.Scripts.DocumentEventArgs args)
+        {
+            //grab a reference to the application
+            if (args.Document != null)
+            {
+                mApp = args.Document.Application;
+                BindScriptsCombo();
+            }
+            else
+            {
+                //this was doc close so just kill the watcher
+                KillWatcher();
+            }
+        }
+
+
+        /// <summary>
+        /// Kills the directory watcher
+        /// </summary>
+        protected void KillWatcher()
+        {
+            if (watcher != null)
+            {
+                watcher.EnableRaisingEvents = false;
+
+                watcher.Dispose();
+                watcher = null;
+            }
+            //enable start btn and disable end
+            btnStartWatcher.Enabled = true;
+            btnStopWatcher.Enabled = false;
+
+            chckBoxCreate.Enabled = true;
+            chckBoxChange.Enabled = true;
+            chckBoxDelete.Enabled = true;
+        }
+
+        /// <summary>
+        /// Starts watching for directory changes
+        /// </summary>
+        protected void StartWatcher()
+        {
+            if (!Directory.Exists(txtDir.Text))
+            {
+                MessageBox.Show("Directory to watch does not exist");
+                return;
+            }
+
+            if (cmbScripts.Items.Count == 0 || (string)cmbScripts.SelectedItem == noScript)
+            {
+                MessageBox.Show("No scripts to call");
+                return;
+            }
+
+            // Create a new FileSystemWatcher and set its properties.
+            watcher = new FileSystemWatcher();
+            watcher.Path = txtDir.Text;
+
+            //Watch for changes
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastAccess | NotifyFilters.LastWrite;
+
+            //watch for all changes
+            watcher.Filter = "*.*";
+
+            //react to file created / changed only
+            if (chckBoxCreate.Checked)
+            {
+                watcher.Created += OnWatchedFileChanged;
+            }
+
+            if (chckBoxChange.Checked)
+            {
+                watcher.Changed += OnWatchedFileChanged;
+                watcher.Renamed += OnWatchedFileChanged;
+            }
+
+            if (chckBoxDelete.Checked)
+            {
+                watcher.Deleted += OnWatchedFileChanged;
+            }
+
+
+            //disable start btn and enable end btn
+            btnStartWatcher.Enabled = false;
+            btnStopWatcher.Enabled = true;
+
+            chckBoxCreate.Enabled = false;
+            chckBoxChange.Enabled = false;
+            chckBoxDelete.Enabled = false;
+
+            //start the watcher
+            watcher.EnableRaisingEvents = true;
+        }
+
+        
+
+        // Define the event handlers. 
+        private void OnWatchedFileChanged(object source, FileSystemEventArgs e)
+        {
+            if (mApp == null || mApp.ActiveDocument == null) return;
+
+            
+            //get a component to use for data dump
+            var dump = GetDumpComp();
+
+            //dump changes
+            dump.Text = e.Name + "|" + e.FullPath + "|" + e.ChangeType;
+
+            //trigger a script
+            var s = GetComponent<M.Script>(callbackScript ?? string.Empty);
+            if (s != null)
+            {
+                s.Run();
+            }
+        }
+
+        /// <summary>
+        /// file watcher notifications manifold component name
+        /// </summary>
+        private string watcherNotificationsComp = "FileWatcherNotifications";
+
+        /// <summary>
+        /// Gets a notifications dump component
+        /// </summary>
+        /// <returns></returns>
+        private M.Comments GetDumpComp()
+        {
+            M.Document mDoc = mApp.ActiveDocument;
+
+            var dumpCompName = txtDumpName.Text;
+            if (string.IsNullOrEmpty(dumpCompName))
+            {
+                dumpCompName = watcherNotificationsComp;
+                txtDumpName.Text = watcherNotificationsComp;
+            }
+
+            M.Comments dump = GetComponent<M.Comments>(dumpCompName);
+            
+            if (dump == null)
+            {
+                dump = mDoc.NewComments(dumpCompName, true);
+            }
+
+            return dump;
+        }
+
+        /// <summary>
+        /// Gets a manifold component
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private T GetComponent<T>(string name)
+        {
+            T output = default(T);
+
+            M.Document mDoc = mApp.ActiveDocument;
+
+            foreach (M.Component c in mDoc.ComponentSet)
+            {
+                if (c.Name == name)
+                {
+                    if (c.Type == GetComponentType<T>())
+                    {
+                        output = (T)c;
+                    }
+                    break;
+                }
+            }
+            return output;
+        }
+
+        private M.ComponentType GetComponentType<T>()
+        {
+            M.ComponentType output = M.ComponentType.ComponentNull;
+
+            if (typeof(M.Comments) == typeof(T))
+            {
+                output = M.ComponentType.ComponentComments;
+            }
+            else if (typeof(M.Script) == typeof(T))
+            {
+                output = M.ComponentType.ComponentScript;
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// start watcher callback
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStartWatcher_Click(object sender, EventArgs e)
+        {
+            StartWatcher();
+        }
+        
+        /// <summary>
+        /// stop watcher btn click callback
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStopWatcher_Click(object sender, EventArgs e)
+        {
+            KillWatcher();
+        }
+       
+
+        /// <summary>
+        /// browse dir btn click callback
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBrowseDir_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                dlg.Description = "Pisk a directory to watch for changes";
+                dlg.SelectedPath = txtDir.Text;
+
+                var result = dlg.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    txtDir.Text = dlg.SelectedPath;
+                }
+            }
+        }
+    }
+}
